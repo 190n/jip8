4639d7b42a29de012f0864009cce013aff5f07ac
Ben Grant <benjamin.j.grant@gmail.com>
2024-05-31 03:16:06 -0700

    mov reg, imm test

--- src/x86_64/assemble.zig
+++ src/x86_64/assemble.zig
@@ -59,9 +59,9 @@ pub fn binOpRegInOpcodeImm(
     }
     try code_buf.append(opcode | @as(u8, dst.num()));
     switch (dst.width()) {
-        .byte_h, .byte_l => try code_buf.append(@bitCast(@as(i8, @intCast(val)))),
-        .word => try code_buf.appendSlice(std.mem.asBytes(&@as(i16, @intCast(val)))),
-        .dword => try code_buf.appendSlice(std.mem.asBytes(&@as(i32, @intCast(val)))),
+        .byte_h, .byte_l => try code_buf.append(@bitCast(@as(i8, @truncate(val)))),
+        .word => try code_buf.appendSlice(std.mem.asBytes(&@as(i16, @truncate(val)))),
+        .dword => try code_buf.appendSlice(std.mem.asBytes(&@as(i32, @truncate(val)))),
         .qword => try code_buf.appendSlice(std.mem.asBytes(&val)),
     }
 }
@@ -87,9 +87,9 @@ pub fn movRegImm(code_buf: *CodeBuf, dst: Reg, val: i64) !void {
             try code_buf.append(0xc7);
             try code_buf.append(@bitCast(ModRM.register(dst, Reg.numbered32(0))));
             try code_buf.appendSlice(std.mem.asBytes(&v));
+            return;
         }
     }
-
     return binOpRegInOpcodeImm(
         code_buf,
         switch (dst.width()) {
@@ -101,62 +101,91 @@ pub fn movRegImm(code_buf: *CodeBuf, dst: Reg, val: i64) !void {
     );
 }
 
+fn testResultMatches(
+    dir: std.fs.Dir,
+    comptime expected_fmt: []const u8,
+    expected_args: anytype,
+    comptime actual_func: anytype,
+    func_args: anytype,
+) !void {
+    var arena = std.heap.ArenaAllocator.init(std.testing.allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    var code_buf = std.ArrayListAligned(u8, std.mem.page_size).init(allocator);
+
+    var asm_file = try dir.createFile("code.S", .{});
+    defer asm_file.close();
+    try asm_file.writeAll(".intel_syntax\n");
+    try asm_file.writer().print(expected_fmt, expected_args);
+
+    const clang_result = try std.process.Child.run(.{
+        .allocator = allocator,
+        .argv = &.{ "zig", "cc", "-target", "x86_64-linux-gnu", "-c", "code.S" },
+        .cwd_dir = dir,
+    });
+
+    if (clang_result.term.Exited == 0) {
+        // our assembler should succeed
+        const objcopy_result = try std.process.Child.run(.{
+            .allocator = allocator,
+            .argv = &.{ "zig", "objcopy", "-O", "binary", "code.o", "code.bin" },
+            .cwd_dir = dir,
+        });
+        try std.testing.expectEqual(0, objcopy_result.term.Exited);
+
+        try @call(.auto, actual_func, .{&code_buf} ++ func_args);
+
+        var bin_file = try dir.openFile("code.bin", .{});
+        defer bin_file.close();
+        var read_buf: [64]u8 = undefined;
+        const size = try bin_file.readAll(&read_buf);
+
+        try std.testing.expectEqualSlices(u8, read_buf[0..size], code_buf.items);
+    } else {
+        // our assembler should fail
+        const did_error = if (@call(.auto, actual_func, .{&code_buf} ++ func_args))
+            false
+        else |_|
+            true;
+        try std.testing.expect(did_error);
+    }
+}
+
 test "movRegReg" {
     var tmp_dir = std.testing.tmpDir(.{});
     defer tmp_dir.cleanup();
-
-    var asm_file = try tmp_dir.dir.createFile("code.S", .{});
-    var code_buf = std.ArrayListAligned(u8, std.mem.page_size).init(std.testing.allocator);
-    defer code_buf.deinit();
-
-    var read_buf: [16]u8 = undefined;
 
     const regs = [_]Reg{ .al, .ah, .ax, .eax, .rax, .spl, .r8b, .r8w, .r8d, .r8 };
 
     for (regs) |r1| {
         for (regs) |r2| {
-            try asm_file.seekTo(0);
-            try asm_file.setEndPos(0);
-            try asm_file.writer().print(
-                \\.intel_syntax
-                \\mov {s}, {s}
-            , .{ @tagName(r1), @tagName(r2) });
+            try testResultMatches(
+                tmp_dir.dir,
+                "mov {s}, {s}",
+                .{ @tagName(r1), @tagName(r2) },
+                movRegReg,
+                .{ r1, r2 },
+            );
+        }
+    }
+}
 
-            const clang_result = try std.process.Child.run(.{
-                .allocator = std.testing.allocator,
-                .argv = &.{ "zig", "cc", "-target", "x86_64-linux-gnu", "-c", "code.S" },
-                .cwd_dir = tmp_dir.dir,
-            });
-            defer std.testing.allocator.free(clang_result.stderr);
-            defer std.testing.allocator.free(clang_result.stdout);
+test "movRegImm" {
+    var tmp_dir = std.testing.tmpDir(.{});
+    defer tmp_dir.cleanup();
 
-            if (clang_result.term.Exited == 0) {
-                // our assembler should succeed
-                const objcopy_result = try std.process.Child.run(.{
-                    .allocator = std.testing.allocator,
-                    .argv = &.{ "zig", "objcopy", "-O", "binary", "code.o", "code.bin" },
-                    .cwd_dir = tmp_dir.dir,
-                });
-                defer std.testing.allocator.free(objcopy_result.stderr);
-                defer std.testing.allocator.free(objcopy_result.stdout);
-                try std.testing.expectEqual(0, objcopy_result.term.Exited);
-
-                code_buf.clearRetainingCapacity();
-                try movRegReg(&code_buf, r1, r2);
-
-                var bin_file = try tmp_dir.dir.openFile("code.bin", .{});
-                defer bin_file.close();
-                const size = try bin_file.readAll(&read_buf);
-
-                try std.testing.expectEqualSlices(u8, read_buf[0..size], code_buf.items);
-            } else {
-                // our assembler should fail
-                const did_error = if (movRegReg(&code_buf, r1, r2))
-                    false
-                else |_|
-                    true;
-                try std.testing.expect(did_error);
-            }
+    const regs = [_]Reg{ .al, .ah, .ax, .eax, .rax, .spl, .r8b, .r8w, .r8d, .r8 };
+    const immediates = [_]i64{ 0x01, 0x0123, 0x01234567, 0x0123456789abcdef };
+    for (regs) |r| {
+        for (immediates) |i| {
+            try testResultMatches(
+                tmp_dir.dir,
+                "mov {s}, 0x{x}",
+                .{ @tagName(r), i },
+                movRegImm,
+                .{ r, i },
+            );
         }
     }
 }
