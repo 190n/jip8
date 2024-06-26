827d07e19f42adfc3cdd94eadf7c64c00c4cbca8
Ben Grant <benjamin.j.grant@gmail.com>
2024-06-26 06:44:21 -0700

    build and run tests for many targets

--- build.zig
+++ build.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+const Query = std.Target.Query;
+
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
@@ -50,18 +52,28 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    b.installArtifact(exe_unit_tests);
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+
+    for ([_]Query{
+        Query.parse(.{ .arch_os_abi = "x86_64-linux-gnu" }) catch unreachable,
+        Query.parse(.{ .arch_os_abi = "riscv64-linux-gnu", .cpu_features = "baseline_rv64" }) catch unreachable,
+        Query.parse(.{ .arch_os_abi = "riscv64-linux-gnu", .cpu_features = "baseline_rv64+c" }) catch unreachable,
+    }) |query| {
+        const exe_unit_tests = b.addTest(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.resolveTargetQuery(query),
+            .name = b.fmt("test-{s}-{s}", .{
+                query.zigTriple(b.allocator) catch @panic("OOM"),
+                query.serializeCpuAlloc(b.allocator) catch @panic("OOM"),
+            }),
+        });
+
+        const install = b.addInstallArtifact(exe_unit_tests, .{});
+        const run = b.addRunArtifact(exe_unit_tests);
+        test_step.dependOn(&install.step);
+        test_step.dependOn(&run.step);
+    }
 }
